5b674b56c5255199ed096801c70b3b13f7a5d715
Martin Jaros <martinjaros88@gmail.com>
2024-12-27 20:58:01 +0100

    Add functions for retrieving trash and normal notes

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { collectionData ,Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
 
-  constructor() { }
+  items$;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.items$ = collectionData(this.getTrashRef());
+  }
+
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getNormalRef() {
+  return collection(this.firestore, 'notes');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId )
+  }
 }
